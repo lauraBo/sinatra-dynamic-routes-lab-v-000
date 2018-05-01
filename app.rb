7a3dd5bf9e83acfb056b768ed3f91a499adaa2a1
Laura Bodell <bodelllaura@gmail.com>
2018-05-01 20:24:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -30,8 +30,8 @@ get '/reversename/:name' do
   end
 
  get '/:operation/:number1/:number2' do
-   @num1 = params[:number1]
-   @num2 = params[:number2]
+   @number1 = params[:number1]
+   @number2 = params[:number2]
    
   
   
